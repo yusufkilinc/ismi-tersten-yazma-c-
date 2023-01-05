172b6af33871fbf88bd96ba6774c6dc9734663d9
yusufkilinc <119483659+yusufkilinc@users.noreply.github.com>
2023-01-05 12:11:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi="yusuf";
+            for (int i= adi.Length-1; i >= 0 ; i--)
+            {
+                System.Console.WriteLine(adi[i]); 
+            }
+        }
+    }
+}
